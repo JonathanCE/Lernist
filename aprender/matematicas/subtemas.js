8d27aa9fef1fc8072bb7c906b08d8a0996fc9bbf
JonathanCE <jonatha.CE.2025@protonmail.com>
2020-09-30 01:29:11 -0500

    esta es la version con el diseño original que tenia pensado para eschool

---
+++ aprender/matematicas/subtemas.js
@@ -0,0 +1,32 @@
+function myFunction() {
+    var x = document.getElementById("myNav");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+  } 
+
+function one(){ var one = document.querySelector(".one")
+  one.classList.toggle("active") }  
+
+  function two(){ var two = document.querySelector(".two")
+  two.classList.toggle("active") }  
+
+  function three(){ var three = document.querySelector(".three")
+  three.classList.toggle("active") }  
+
+  function four(){ var four = document.querySelector(".four")
+  four.classList.toggle("active") } 
+  
+  function five(){ var five = document.querySelector(".five")
+  five.classList.toggle("active") }  
+
+  function six(){ var six = document.querySelector(".six")
+  six.classList.toggle("active") }  
+
+  function seven(){ var seven = document.querySelector(".seven")
+  seven.classList.toggle("active") } 
+  
+  function eight(){ var eight = document.querySelector(".eight")
+  eight.classList.toggle("active") }  
