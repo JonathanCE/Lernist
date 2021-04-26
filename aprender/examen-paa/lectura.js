00c132fa89068425e5c06eb5fe564acd0cd97d29
JonathanCE <jonatha.CE.2025@protonmail.com>
2021-04-26 02:33:17 -0500

    Se cambió la foto de miniatura en el navegador por el nuevo logo de lernist, y tambien se completó el nuevo diseño para los acordiones de temas, ahora diseñado completamente por mi

---
+++ aprender/examen-paa/lectura.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var x = document.getElementById("myNav");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+  } 
+
+/* const lecciones = document.getElementsByClassName ('leccion');
+
+  for (i = 0; i<lecciones.length; i++) {
+    lecciones[i].addEventListener('click', function(){
+      this.classList.toggle('active')
+    })
+  } */
