f28bc50b9f82023d58226ada968837e3e7a7b93c
JonathanCE <jonatha.CE.2025@protonmail.com>
2021-06-04 15:48:48 -0500

    Ahora los links y nombres en el navbar y el footer se pueden controlar mediante JQuery

--- main.js
+++ main.js
@@ -8,6 +8,24 @@
 } */
 
 $(document).ready(function() {
+
+  $("#logoNavbar").attr("src", "i_temas/Logo Lernist.png")
+  $(".Inicio").text("Inicio");
+  $("#aprenderLink").text("Aprender");
+  $("#enseñarLink").hide();
+  $("#enseñarLink").text("Enseñar");
+
+  $("#fraseFooter").text("Aprende cosas Increíbles, con contenido Increíble.");
+  $("#footerFacebook").attr("href", "https://www.facebook.com/LernistOficial");
+  $("#footerTwitter").attr("href", "https://twitter.com/Lernist_Oficial");
+  $("#footerInstagram").attr("href", "https://www.instagram.com/Lernist_Oficial/");
+  $("#footerContacto").attr("href", "contacto.html");
+  $("#footerContacto").text("Contacto");
+  $("#footerNosotros").attr("href", "nosotros.html");
+  $("#footerNosotros").text("Nosotros");
+  $("#footerAnunciantes").attr("href", "");
+  $("#footerAnunciantes").text("Anunciantes");
+
   $("#menuHamburguesa").click(function() {
     $(".menuPrincipal").slideToggle(400);
     $(".bar1").toggleClass("change-bar1");
