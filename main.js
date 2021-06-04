dd25a8a26b5f2b83518bd52ecb8a5f48e7ba61f3
JonathanCE <jonatha.CE.2025@protonmail.com>
2021-06-04 16:48:10 -0500

    EL NUEVO NAVBAR AHORA ESTA EN TODO LERNIST, y tambien se hicieron algunas correcciones en el codigo del mismo

--- main.js
+++ main.js
@@ -9,11 +9,14 @@
 
 $(document).ready(function() {
 
+  $("#homepageLink").attr("href", "index.html");
   $("#logoNavbar").attr("src", "i_temas/Logo Lernist.png")
   $(".Inicio").text("Inicio");
   $("#aprenderLink").text("Aprender");
+  $("#aprenderLink").attr("href", "aprender.html")
   $("#enseñarLink").hide();
   $("#enseñarLink").text("Enseñar");
+  $("#enseñarLink").attr("href", "enseñar.html")
 
   $("#fraseFooter").text("Aprende cosas Increíbles, con contenido Increíble.");
   $("#footerFacebook").attr("href", "https://www.facebook.com/LernistOficial");
@@ -23,8 +26,10 @@ $(document).ready(function() {
   $("#footerContacto").text("Contacto");
   $("#footerNosotros").attr("href", "nosotros.html");
   $("#footerNosotros").text("Nosotros");
+  $("#footerNosotros").hide();
   $("#footerAnunciantes").attr("href", "");
   $("#footerAnunciantes").text("Anunciantes");
+  $("#footerAnunciantes").hide();
 
   $("#menuHamburguesa").click(function() {
     $(".menuPrincipal").slideToggle(400);
--- temas.js
+++ temas.js
@@ -1,11 +1,34 @@
-function myFunction() {
-    var x = document.getElementById("myNav");
-    if (x.className === "nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav";
-    }
-  } 
+$(document).ready(function() {
+
+  $("#homepageLink").attr("href", "/index.html");
+  $("#logoNavbar").attr("src", "/i_temas/Logo Lernist.png")
+  $(".Inicio").text("Inicio");
+  $("#aprenderLink").text("Aprender");
+  $("#aprenderLink").attr("href", "/aprender.html")
+  $("#enseñarLink").hide();
+  $("#enseñarLink").text("Enseñar");
+  $("#enseñarLink").attr("href", "/enseñar.html")
+
+  $("#fraseFooter").text("Aprende cosas Increíbles, con contenido Increíble.");
+  $("#footerFacebook").attr("href", "https://www.facebook.com/LernistOficial");
+  $("#footerTwitter").attr("href", "https://twitter.com/Lernist_Oficial");
+  $("#footerInstagram").attr("href", "https://www.instagram.com/Lernist_Oficial/");
+  $("#footerContacto").attr("href", "/contacto.html");
+  $("#footerContacto").text("Contacto");
+  $("#footerNosotros").attr("href", "/nosotros.html");
+  $("#footerNosotros").text("Nosotros");
+  $("#footerNosotros").hide();
+  $("#footerAnunciantes").attr("href", "");
+  $("#footerAnunciantes").text("Anunciantes");
+  $("#footerAnunciantes").hide();
+
+  $("#menuHamburguesa").click(function() {
+    $(".menuPrincipal").slideToggle(400);
+    $(".bar1").toggleClass("change-bar1");
+    $(".bar2").toggleClass("change-bar2");
+    $(".bar3").toggleClass("change-bar3");
+  });
+}); 
 
 const subtemas = document.getElementsByClassName ('subtema');
 
