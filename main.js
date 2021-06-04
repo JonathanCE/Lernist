dd03296dccf72730724c6933853115c99575fd60
JonathanCE <jonatha.CE.2025@protonmail.com>
2021-06-04 00:47:50 -0500

    Se rediseño la navbar: ahora yo diseñe el efecto en moviles para mostrar y ocultar los otros elementos de la navbar, ademas añadí un efecto al boton de hamburguesa

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-function myFunction() {
-    var x = document.getElementById("myNav");
-    if (x.className === "nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav";
-    }
-  } 
+/* function myFunction() {
+  var x = document.getElementById("myNav");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+} */
+
+$(document).ready(function() {
+  $("#menuHamburguesa").click(function() {
+    $(".menuPrincipal").slideToggle(400);
+    $(".bar1").toggleClass("change-bar1");
+    $(".bar2").toggleClass("change-bar2");
+    $(".bar3").toggleClass("change-bar3");
+  });
+});
