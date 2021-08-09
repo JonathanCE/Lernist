43565c48abf328b464712f6343ee13c5cb2ac71c
JonathanCE <jonatha.CE.2025@protonmail.com>
2021-08-09 02:52:32 -0500

    Se hicieron multiples cosas: para empezar, se cambio el nombre de archivos css y js de temas a materias, y de subtemas a cursos. Se cambio el codigo en los archivos html que enlazaban a estos archivos y ahora enlazan a los nuevos nombres. Tambien se modifico radicalmente los archivos html y js modelo para los videos de nuevos cursos. Y por ultimo, se comenzo a diseñar el curso de abc de matematicas, donde se crearon la mitad de los html para los videos, se comenzo a diseñar el primer video y el js que controlara los archivos de videos del curso

--- JS-de-videos-de-cursos/abc-de-matematicas.js
+++ JS-de-videos-de-cursos/abc-de-matematicas.js
@@ -75,41 +75,118 @@ $(document).ready(function() {
   $("#VideosMensajeInicio").text("¡El curso comienza aquí!");
   $("#VideosMensajeFinal").text("¡Haz terminado el curso!");
 
-  // TITULO DE CURSO
-  // TITULO DE CURSO
+  // ABC DE MATEMATICAS
+  // ABC DE MATEMATICAS
 
   // LECCIONES
-  $("#idNameLeccion").text("Parte o Leccion");
+  $("#leccion-jerarquiaOperaciones").text("Jerarquía de las Operaciones");
+  $("#leccion-leySignos").text("Leyes de los Signos");
+  $("#leccion-fracciones").text("Fracciones");
+  $("#leccion-decimales").text("Números Decimales");
+  $("#leccion-mcm").text("Mínimo común multiplo y Máximo común divisor");
+  $("#leccion-potencias").text("Potencias");
+  $("#leccion-raices").text("Raiz cuadrada");
+  $("#leccion-porcentajes").text("Porcentajes");
+  $("#leccion-mediaModaMediana").text("Media, moda y mediana");
+  $("#leccion-probabilidad").text("Probabilidad simple");
+  $("#leccion-perimetroArea").text("Perímetro y Area de figuras básicas");
+  $("#leccion-volumen").text("Volúmenes de prismas principales");
+  $("#leccion-angulos").text("Ángulos y figuras");
+  $("#leccion-lenguajeAlgebraico").text("Lenguaje Algebraico");
+  $("#leccion-terminosSemejantes").text("Términos semejantes");
+  $("#leccion-ecuaciones").text("Ecuaciones de primer grado");
+  $("#leccion-planoCartesiano").text("Plano cartesiano y gráficar ecuaciones");
+  $("#leccion-teoremaPitagoras").text("Teorema de Pitágoras");
 
   // TITULOS DE MENU ACORDION (lecciones y partes)
-  $("#tituloAcordion1").text("Titulo de leccion");
-  $("#tituloAcordion2").text("Titulo de leccion");
-  $("#tituloAcordion3").text("Titulo de leccion");
-  $("#tituloAcordion4").text("Titulo de leccion");
-  // Ejemplo:
-  $("#paaVideosMateLeccionAritmetica").text("Aritmética");
+  $("#tituloAcordion1").text("Jerarquía de las Operaciones");
+  $("#tituloAcordion2").text("Leyes de los Signos");
+  $("#tituloAcordion3").text("Fracciones");
+  $("#tituloAcordion4").text("Números Decimales");
+  $("#tituloAcordion5").text("Mínimo común multiplo y Máximo común divisor");
+  $("#tituloAcordion6").text("Potencias");
+  $("#tituloAcordion7").text("Raiz cuadrada");
+  $("#tituloAcordion8").text("Porcentajes");
+  $("#tituloAcordion9").text("Media, moda y mediana");
+  $("#tituloAcordion10").text("Probabilidad simple");
+  $("#tituloAcordion11").text("Perímetro y Area de figuras básicas");
+  $("#tituloAcordion12").text("Volúmenes de prismas principales");
+  $("#tituloAcordion13").text("Ángulos y figuras");
+  $("#tituloAcordion14").text("Lenguaje Algebraico");
+  $("#tituloAcordion15").text("Términos semejantes");
+  $("#tituloAcordion16").text("Ecuaciones de primer grado");
+  $("#tituloAcordion17").text("Plano cartesiano y gráficar ecuaciones");
+  $("#tituloAcordion18").text("Teorema de Pitágoras");
 
   // LINKS DE CADA VIDEO | MENU ACORDION
-  // 'NOMBRE DE PARTE DEL CURSO'
-  $("#videoAcordion1").attr("href", "url al video").text("1. ");
-  $("#videoAcordion2").attr("href", "url al video").text("2. ");
-  $("#videoAcordion3").attr("href", "url al video").text("3. ");
-
-  // Sig. Leccion
-  $("#videoAcordion4").attr("href", "url al video").text("4. Tipos de discurso: narrativo, descriptivo, argumentativo");
-
-  // OTRA PARTE (en caso que se necesite)
-  $("#paaVideosMate1Menu").attr("href", "url al video").text("1. Jerarquía de las Operaciones | Super fácil");
-  $("#paaVideosMate2Menu").attr("href", "url al video").text("2. Ley de los signos | Super fácil");
-  $("#paaVideosMate3Menu").attr("href", "url al video").text("3. Si te parecen difícil las fracciones debes ver este video");
-  $("#paaVideosMate4Menu").attr("href", "url al video").text("4. Operaciones combinadas con fracciones");
-  $("#paaVideosMate5Menu").attr("href", "url al video").text("5. Números decimales introducción");
+  // Jerarquia de las operaciones
+  $("#videoAcordion1").attr("href", "video1.html").text("1. ");
+  $("#videoAcordion2").attr("href", "video2.html").text("2. ");
+  $("#videoAcordion3").attr("href", "video3.html").text("3. ");
+  $("#videoAcordion4").attr("href", "video4.html").text("4. ");
+  $("#videoAcordion5").attr("href", "video5.html").text("5. ");
+  // Leyes de los Signos
+  $("#videoAcordion6").attr("href", "video6.html").text("6. ");
+  $("#videoAcordion7").attr("href", "video7.html").text("7. ");
+  // Fracciones
+  $("#videoAcordion8").attr("href", "video8.html").text("8. ");
+  $("#videoAcordion9").attr("href", "video9.html").text("9. ");
+  $("#videoAcordion10").attr("href", "video10.html").text("10. ");
+  $("#videoAcordion11").attr("href", "video11.html").text("11. ");
+  $("#videoAcordion12").attr("href", "video12.html").text("12. ");
+  $("#videoAcordion13").attr("href", "video13.html").text("13. ");
+  $("#videoAcordion14").attr("href", "video14.html").text("14. ");
+  $("#videoAcordion15").attr("href", "video15.html").text("15. ");
+  $("#videoAcordion16").attr("href", "video16.html").text("16. ");
+  $("#videoAcordion17").attr("href", "video17.html").text("17. ");
+  // Numeros decimales
+  $("#videoAcordion18").attr("href", "video18.html").text("18. ");
+  $("#videoAcordion19").attr("href", "video19.html").text("19. ");
+  $("#videoAcordion20").attr("href", "video20.html").text("20. ");
+  $("#videoAcordion21").attr("href", "video21.html").text("21. ");
+  $("#videoAcordion22").attr("href", "video22.html").text("22. ");
+  $("#videoAcordion23").attr("href", "video23.html").text("23. ");
+  $("#videoAcordion24").attr("href", "video24.html").text("24. ");
+  $("#videoAcordion25").attr("href", "video25.html").text("25. ");
+  // mcm y MCD
+  $("#videoAcordion26").attr("href", "video26.html").text("26. ");
+  $("#videoAcordion27").attr("href", "video27.html").text("27. ");
+  $("#videoAcordion28").attr("href", "video28.html").text("28. ");
+  $("#videoAcordion29").attr("href", "video29.html").text("29. ");
+  // Potencias
+  $("#videoAcordion30").attr("href", "video30.html").text("30. ");
+  $("#videoAcordion31").attr("href", "video31.html").text("31. ");
+  $("#videoAcordion32").attr("href", "video32.html").text("32. ");
+  // Raiz cuadrada
+  $("#videoAcordion33").attr("href", "video33.html").text("33. ");
+  $("#videoAcordion34").attr("href", "video34.html").text("34. ");
+  $("#videoAcordion35").attr("href", "video35.html").text("35. ");
+  // Porcentajes
+  $("#videoAcordion36").attr("href", "video36.html").text("36. ");
+  $("#videoAcordion37").attr("href", "video37.html").text("37. ");
+  $("#videoAcordion38").attr("href", "video38.html").text("38. ");
+  $("#videoAcordion39").attr("href", "video39.html").text("39. ");
+  $("#videoAcordion40").attr("href", "video40.html").text("40. ");
+  // Media, moda y mediana
+  $("#videoAcordion41").attr("href", "video41.html").text("41. ");
+  $("#videoAcordion42").attr("href", "video42.html").text("42. ");
+  $("#videoAcordion43").attr("href", "video43.html").text("43. ");
+  // Probabilidad simple
+  $("#videoAcordion44").attr("href", "video44.html").text("44. ");
+  $("#videoAcordion45").attr("href", "video45.html").text("45. ");
+  // Perimetro y Area
+  $("#videoAcordion46").attr("href", "video45.html").text("46. ");
 
   // DATOS DE CADA VIDEO
   $("#tituloVideo1").text("1. ");
-  $("#urlVideo1").attr("src", "https://www.youtube.com/embed/TPmlD6demPk?cc_lang_pref=es&cc_load_policy=1"); // url del video
+  $("#urlVideo1").attr("src", "https://www.youtube.com/embed/XV5PiV2-91U?cc_lang_pref=es&cc_load_policy=1"); // url del video
   $("#video1LinkAnterior").attr("href", ""); // video anterior
   $("#video1LinkSiguiente").attr("href", ""); // video siguiente
+
+  $("#tituloVideo2").text("2. ");
+  $("#urlVideo2").attr("src", "?cc_lang_pref=es&cc_load_policy=1"); // url del video
+  $("#video2LinkAnterior").attr("href", ""); // video anterior
+  $("#video2LinkSiguiente").attr("href", ""); // video siguiente
 
   // ACERCA DE
   $("#creadorVideo").text("Creador del video");
@@ -145,8 +222,8 @@ $(document).ready(function() {
   // descripcion de cada video
   $("#descripcionCursoVideo1Texto").text("-");
 
-  // TITULO DEL CURSO
-  // TITULO DEL CURSO
+  // ABC DE MATEMATICAS
+  // ABC DE MATEMATICAS
 
   // ANIMACION PARA MENU VIDEO-INFO
   $(".video-info-title").click(function() {
@@ -273,39 +350,3 @@ $(document).ready(function() {
     $(".vi30").slideToggle(400);
   });
 });
-
-
-
-// BLOQUES DE CODIGO UTILES
-
-// TITULOS DE MENU ACORDION (lecciones y partes) - hasta 30 lecciones
-$("#tituloAcordion1").text("");
-$("#tituloAcordion2").text("");
-$("#tituloAcordion3").text("");
-$("#tituloAcordion4").text("");
-$("#tituloAcordion5").text("");
-$("#tituloAcordion6").text("");
-$("#tituloAcordion7").text("");
-$("#tituloAcordion8").text("");
-$("#tituloAcordion9").text("");
-$("#tituloAcordion10").text("");
-$("#tituloAcordion11").text("");
-$("#tituloAcordion12").text("");
-$("#tituloAcordion13").text("");
-$("#tituloAcordion14").text("");
-$("#tituloAcordion15").text("");
-$("#tituloAcordion16").text("");
-$("#tituloAcordion17").text("");
-$("#tituloAcordion18").text("");
-$("#tituloAcordion19").text("");
-$("#tituloAcordion20").text("");
-$("#tituloAcordion21").text("");
-$("#tituloAcordion22").text("");
-$("#tituloAcordion23").text("");
-$("#tituloAcordion24").text("");
-$("#tituloAcordion25").text("");
-$("#tituloAcordion26").text("");
-$("#tituloAcordion27").text("");
-$("#tituloAcordion28").text("");
-$("#tituloAcordion29").text("");
-$("#tituloAcordion30").text("");
