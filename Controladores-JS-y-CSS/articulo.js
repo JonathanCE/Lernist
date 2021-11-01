7c4f98e9173380e7f0db94f6db25ffa624eab54e
JonathanCE <jonatha.CE.2025@protonmail.com>
2021-10-31 21:48:09 -0600

    El articulo plantilla para ser usado como esqueleto ya esta listo, junto con sus respectivos archivos js y css. Tambien, ahora hay una carpeta donde iran todos los archivos css y js que controlen una pagina o paginas en lernist, para tenerlos agrupados en un lugar

---
+++ Controladores-JS-y-CSS/articulo.js
@@ -0,0 +1,77 @@
+var anunciosSuperiores = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("anuncios-superiores");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  anunciosSuperiores++;
+  if (anunciosSuperiores > x.length) {anunciosSuperiores = 1}
+  x[anunciosSuperiores-1].style.display = "block";
+  setTimeout(carousel, 7000); // Change image every 7 seconds
+}
+
+var anunciosSuperioresMovil = 0;
+carouselMovil();
+
+function carouselMovil() {
+  var i;
+  var x = document.getElementsByClassName("anuncios-superiores-movil");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  anunciosSuperioresMovil++;
+  if (anunciosSuperioresMovil > x.length) {anunciosSuperioresMovil = 1}
+  x[anunciosSuperioresMovil-1].style.display = "block";
+  setTimeout(carouselMovil, 7000); // Change image every 7 seconds
+}
+
+// INDICE
+// ELEMENTOS MADRE: linea 16
+// 
+
+$(document).ready(function() {
+
+  // PRIMORDIALES
+  $("#homepageLink").attr("href", "/");
+  $("#logoNavbar").attr("src", "/i_temas/Logo Lernist.png")
+  $(".Inicio").text("Inicio");
+  $("#aprenderLink").text("Aprender");
+  $("#aprenderLink").attr("href", "/aprender.html")
+  $("#articulosLink").text("Artículos");
+  $("#articulosLink").attr("href", "/articulos.html")
+  $("#enseñarLink").hide();
+  $("#enseñarLink").text("Enseñar");
+  $("#enseñarLink").attr("href", "/enseñar.html")
+
+  $("#fraseFooter").text("Aprende cosas Increíbles, con contenido Increíble.");
+  $("#footerFacebook").attr("href", "https://www.facebook.com/LernistOficial");
+  $("#footerTwitter").attr("href", "https://twitter.com/Lernist_Oficial");
+  $("#footerInstagram").attr("href", "https://www.instagram.com/Lernist_Oficial/");
+  $("#footerContacto").attr("href", "/contacto.html");
+  $("#footerContacto").text("Contacto");
+  $("#footerNosotros").attr("href", "/nosotros.html");
+  $("#footerNosotros").text("Nosotros");
+  $("#footerAnunciantes").attr("href", "");
+  $("#footerAnunciantes").text("Anunciantes");
+  $("#footerAnunciantes").hide();
+
+  $("#menuHamburguesa").click(function() {
+    $(".menuPrincipal").slideToggle(400);
+    $(".bar1").toggleClass("change-bar1");
+    $(".bar2").toggleClass("change-bar2");
+    $(".bar3").toggleClass("change-bar3");
+  });
+
+  // ELEMENTOS MADRE
+  $("#titulo-lecciones").text("Navega por el curso:");
+  $("#VideosBotonPrevious").attr("src", "/i_temas/previous.png");
+  $("#VideosBotonPreviousText").text("Anterior");
+  $("#VideosBotonNext").attr("src", "/i_temas/next.png");
+  $("#VideosBotonNextText").text("Siguiente");
+  $("#VideosMensajeInicio").text("¡El curso comienza aquí!");
+  $("#VideosMensajeFinal").text("¡Haz terminado el curso!");
+  
+});
