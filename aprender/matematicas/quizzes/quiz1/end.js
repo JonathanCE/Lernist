aca195d0bfd5362b71a8974ceb1456a3a2321a1e
JonathanCE <jonatha.CE.2025@protonmail.com>
2021-12-12 22:07:58 -0600

    Primeros dos quizzes terminados, son de operaciones con fracciones, y se borro el codigo que permite guardar y ver puntajes anteriores, no es necesario

--- aprender/matematicas/quizzes/quiz1/end.js
+++ aprender/matematicas/quizzes/quiz1/end.js
@@ -25,5 +25,5 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('quizName.html');
+    window.location.assign('suma-y-resta-fracciones.html');
 };
