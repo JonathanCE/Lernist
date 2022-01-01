18955cb05cf149603ac0edf0301a3cbd23bc6686
JonathanCE <jonatha.CE.2025@protonmail.com>
2022-01-01 05:12:17 -0600

    Al fin, se termino el generador de certificados, totalmente funcional y ajustable. Ademas, tambien se diseño el codigo para que quizzes, minitests, tests o cualquier tipo de prueba, si se logra un puntaje deseado, aparezca el boton enlace al generador de certificado. Y por ultimo, parece que algolia ahora ya reconoce los quizzes y articulos en sus busqueda, porlo que los campos de busqueda en la pagina de articulos y quizzes fueron habilitados de nuevo, junto con un mensaje debajo para los tips de busqueda

---
+++ certificate-generator/certificate.js
@@ -0,0 +1,45 @@
+const { degrees, PDFDocument, rgb, StandardFonts } = PDFLib
+const nombrePersona = document.getElementById("NombrePersona")
+
+async function nombreEnPdf() {
+    // Fetch an existing PDF document
+    const existingPdfBytes = await fetch("Certificate.pdf").then(res => res.arrayBuffer())
+    // Fetch custom font
+    const fontBytes = await fetch("Roboto-Bold.ttf").then(res => res.arrayBuffer())
+
+    // Load a PDFDocument from the existing PDF bytes
+    const pdfDoc = await PDFDocument.load(existingPdfBytes)
+    // Register the `fontkit` instance
+    pdfDoc.registerFontkit(fontkit)
+
+    // Embed our custom font in the document
+    const customFont = await pdfDoc.embedFont(fontBytes)
+
+    // Get the first page of the document
+    const pages = pdfDoc.getPages()
+    const firstPage = pages[0]
+
+    // Get the width and height of the first page
+    const { width, height } = firstPage.getSize()
+
+    // Create a string of text and measure its width and height in our custom font
+    const text = nombrePersona.value
+    const textSize = 35
+    const textWidth = customFont.widthOfTextAtSize(text, textSize)
+    const textHeight = customFont.heightAtSize(textSize)
+
+    // Draw the string of text on the page
+    firstPage.drawText(text, {
+        x: ( width - textWidth ) / 2,
+        y: 290,
+        size: textSize,
+        font: customFont,
+        color: rgb(0, 0.53, 0.71),
+    })
+
+    // Serialize the PDFDocument to bytes (a Uint8Array)
+    const pdfBytes = await pdfDoc.save()
+
+    // Trigger the browser to download the PDF document
+    download(pdfBytes, "mi_certificado.pdf", "application/pdf");
+}
---
+++ quizzes/geografia/quizzes-de-cursos/descubre-europa-con-luisillo/quiz1/end.js
@@ -0,0 +1,41 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const botonCertificado = document.getElementById('certificadoLink');
+const intentaDenuevo = document.getElementById('intentaloDenuevo');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
+botonCertificado.style.display = 'none';
+intentaDenuevo.style.display = 'none';
+
+if (mostRecentScore >= 20) {
+    botonCertificado.style.display = 'initial';
+}
+else {
+    intentaDenuevo.style.display = 'initial';
+}
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
+
+saveHighScore = (e) => {
+    e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('descubre-europa-con-luisillo.html');
+};
