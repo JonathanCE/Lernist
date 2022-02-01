f188e48c7c48c54b5e2aedf74fd11bdd6e345b16
JonathanCE <jonatha.CE.2025@protonmail.com>
2022-02-01 17:09:22 -0600

    Se añadieron 8 cursos nuevos. Ahora ambos formatos de cursos, insertados y enlistamos, usan un mismo archivo js. Y por ultimo, se edito un poco el mensaje de donacion por PayPal

--- Controladores-JS-y-CSS/curso-insertado-y-enlistado.js
+++ Controladores-JS-y-CSS/curso-insertado-y-enlistado.js
@@ -40,18 +40,31 @@ $(document).ready(function() {
 
   // ACERCA DE
   $("#creadorCurso").text("Creador del curso");
-  $("#creadorCursoTexto").prepend('Los videos fueron creados por el canal de youtube ');
-  $("#creadorCursoTexto").append("Lernist.com no es el propietario de estos videos."
+  $("#creadorCursoYoutubeTexto").prepend('Los videos fueron creados por el canal de youtube ');
+  $("#creadorCursoYoutubeTexto").append("Lernist.com no es el propietario de estos videos."
   + " Distribuímos estos videos mediante la herramienta pública de Youtube que permite insertar"
   + " videos de la plataforma en sitios web externos.");
+
+  $("#creadorCursoExternoTexto").prepend('El curso forma parte del servicio ');
+  $("#creadorCursoExternoTexto").append("Lernist.com no es el propietario de este material."
+  + " Enlistamos este material en nuestra recopilación de cursos y otros programas educativos"
+  + " a lo largo de internet para facilitar a las personas a encontrar los"
+  + " mejores materiales educativos en un único lugar.");
 
   $("#descripcionCurso").prepend("Descripción del curso"); // Descripcion del curso
 
   $("#tipsCurso").text("Tips para el curso"); // Tips para el curso
-  $("#tipsCursoTexto").text("El curso lo puedes tomar directamente desde aquí, y funciona bien tanto"
+  $("#tipsCursoYoutubeTexto").text("El curso lo puedes tomar directamente desde aquí, y funciona bien tanto"
   + " en celular, tablet o computadora. En caso de que tengas tu cuenta"
   + " de youtube iniciada en el navegador, también permitirá que tu progreso"
   + " quede guardado y se muestre. Recomendamos que tomes notas en base a lo que"
+  + " comprendas, ya que te ayudará a entender los conceptos mejor y podrás revisarlas"
+  + " cada vez que necesites. De igual manera, estar en un lugar cómodo y sin"
+  + " distracciones es lo ideal. Puedes ver las lecciones las veces que necesites para que"
+  + " comprendas bien el tema en cuestión, aquí no tienes presión de tiempo o preocuparte"
+  + " porque la/el maestr@ borre el pizarrón, simplemente le regresas o comienzas denuevo.")
+  
+  $("#tipsCursoExternoTexto").text("Recomendamos que tomes notas en base a lo que"
   + " comprendas, ya que te ayudará a entender los conceptos mejor y podrás revisarlas"
   + " cada vez que necesites. De igual manera, estar en un lugar cómodo y sin"
   + " distracciones es lo ideal. Puedes ver las lecciones las veces que necesites para que"
