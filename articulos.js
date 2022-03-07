f1086221dd4d1358fc0e6d471d19620cf46b7c8e
JonathanCE <jonatha.CE.2025@protonmail.com>
2022-03-07 01:57:21 -0600

    Se termino la version funcional de la nueva pagina de articulos y de la pagina de utilidades

---
+++ articulos.js
@@ -0,0 +1,15 @@
+function todos() {
+    $(".finanzas").fadeIn();
+    $(".programacion").fadeIn();
+    $(".negocios").fadeIn();
+    $(".inversiones").fadeIn();
+    $(".diseño").fadeIn();
+};
+
+function matematicas() {
+    $(".matematicas").fadeIn();
+    $(".programacion").fadeOut();
+    $(".negocios").fadeOut();
+    $(".inversiones").fadeOut();
+    $(".diseño").fadeOut();
+};
---
+++ utilidades.js
@@ -0,0 +1,43 @@
+function hide() {
+    $(".utilidad").fadeOut();
+}
+
+function todos() {
+    hide();
+    $(".utilidad").fadeIn();
+};
+
+function finanzas() {
+    hide();
+    $(".finanzas").fadeIn();
+};
+
+function programacion() {
+    hide();
+    $(".programacion").fadeIn();
+};
+
+function negocios() {
+    hide();
+    $(".negocios").fadeIn();
+};
+
+function inversiones() {
+    hide();
+    $(".inversiones").fadeIn();
+};
+
+function diseño() {
+    hide();
+    $(".diseño").fadeIn();
+};
+
+function idiomas() {
+    hide();
+    $(".idiomas").fadeIn();
+};
+
+function productividad() {
+    hide();
+    $(".productividad").fadeIn();
+};
