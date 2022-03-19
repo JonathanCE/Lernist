1ab7dec34a0b9db09192dbe93b5ccf093a062779
JonathanCE <jonatha.CE.2025@protonmail.com>
2022-03-18 20:27:02 -0600

    Se añadieron 3 nuevos articulos y tambien se actualizaron otros al nuevo formato, y se añadieron 2 nuevas utilidades

--- articulos.js
+++ articulos.js
@@ -11,3 +11,28 @@ function matematicas() {
     hide();
     $(".matematicas").fadeIn();
 };
+
+function programacion() {
+    hide();
+    $(".programacion").fadeIn();
+};
+
+function fisica() {
+    hide();
+    $(".fisica").fadeIn();
+};
+
+function negocios() {
+    hide();
+    $(".negocios").fadeIn();
+};
+
+function inversiones() {
+    hide();
+    $(".inversiones").fadeIn();
+};
+
+function educacion() {
+    hide();
+    $(".educacion").fadeIn();
+};
