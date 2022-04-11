eafd9570698eb0f5b213b9129def2854d1684008
JonathanCE <jonatha.CE.2025@protonmail.com>
2022-04-10 23:46:23 -0500

    Se transformo la pagina de colecciones, para ahora mostrar los articulos tendencia en un swiper, y con imagen tambien. Por supuesto, se elimino la vieja version de diseño de trending

---
+++ articulos-swiper.js
@@ -0,0 +1,46 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // Define la cantidad de slides que se muestran
+    // y el separador
+    slidesPerView: 1,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    //breakpoints, para hacer el swiper responsive
+    breakpoints: {
+        470: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        800: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1150: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        }
+    },
+
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});
