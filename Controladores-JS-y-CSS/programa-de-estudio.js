d7fac5ac5b9c1460324f2c19d7aba1d0901787e6
JonathanCE <jonatha.CE.2025@protonmail.com>
2022-05-18 19:59:36 -0500

    Se hicieron notables avances en la pagina de la licenciatura en fisica, y tambien algunos ajustes en colecciones de utilidades, y por ultimo se comenzo el borrador del texto que habla bien del aprendizaje online

--- Controladores-JS-y-CSS/programa-de-estudio.js
+++ Controladores-JS-y-CSS/programa-de-estudio.js
@@ -7,52 +7,19 @@ $(document).ready(function() {
     // ELEMENTOS MADRE
     $(".acordionTabla").hide();
     $(".acordionNormal").hide();
-    $("#datos-video").show();
-  
-    // ACERCA DE
-    $("#creadorCurso").text("Creador del curso");
-    $("#creadorCursoYoutubeTexto").prepend('Los videos fueron creados por el canal de youtube ');
-    $("#creadorCursoYoutubeTexto").append("Lernist.com no es el propietario de estos videos."
-    + " Distribuímos estos videos mediante la herramienta pública de Youtube que permite insertar"
-    + " videos de la plataforma en sitios web externos.");
-  
-    $("#creadorCursoExternoTexto").prepend('El curso forma parte del servicio ');
-    $("#creadorCursoExternoTexto").append("Lernist.com no es el propietario de este material."
-    + " Enlistamos este material en nuestra recopilación de cursos y otros programas educativos"
-    + " a lo largo de internet para facilitar a las personas a encontrar los"
-    + " mejores materiales educativos en un único lugar.");
-  
-    $("#descripcionCurso").prepend("Descripción del curso"); // Descripcion del curso
-  
-    $("#tipsCurso").text("Tips para el curso"); // Tips para el curso
-    $("#tipsCursoYoutubeTexto").text("El curso lo puedes tomar directamente desde aquí, y funciona bien tanto"
-    + " en celular, tablet o computadora. En caso de que tengas tu cuenta"
-    + " de youtube iniciada en el navegador, también permitirá que tu progreso"
-    + " quede guardado y se muestre. Recomendamos que tomes notas en base a lo que"
-    + " comprendas, ya que te ayudará a entender los conceptos mejor y podrás revisarlas"
-    + " cada vez que necesites. De igual manera, estar en un lugar cómodo y sin"
-    + " distracciones es lo ideal. Puedes ver las lecciones las veces que necesites para que"
-    + " comprendas bien el tema en cuestión, aquí no tienes presión de tiempo o preocuparte"
-    + " porque la/el maestr@ borre el pizarrón, simplemente le regresas o comienzas denuevo.")
-    
-    $("#tipsCursoExternoTexto").text("Recomendamos que tomes notas en base a lo que"
-    + " comprendas, ya que te ayudará a entender los conceptos mejor y podrás revisarlas"
-    + " cada vez que necesites. De igual manera, estar en un lugar cómodo y sin"
-    + " distracciones es lo ideal. Puedes ver las lecciones las veces que necesites para que"
-    + " comprendas bien el tema en cuestión, aquí no tienes presión de tiempo o preocuparte"
-    + " porque la/el maestr@ borre el pizarrón, simplemente le regresas o comienzas denuevo.")
+    //$("#info-carrera-content").show();
   
     // ANIMACION PARA MENU VIDEO-INFO
-    $("#video-info-title").click(function() {
-      $("#datos-video").slideToggle(400);
+    $("#info-carrera").click(function() {
+      $("#info-carrera-content").slideToggle(400);
       $("#infoBtnMas").toggle();
       $("#infoBtnMenos").toggle();
     });
     // ANIMACION PARA MENU INSTRUCCIONES
-    $("#curso-instrucciones-title").click(function() {
-      $("#curso-instrucciones-content").slideToggle(400);
-      $("#instruccionesBtnMas").toggle();
-      $("#instruccionesBtnMenos").toggle();
+    $("#carrera-recursos").click(function() {
+      $("#carrera-recursos-content").slideToggle(400);
+      $("#carrera-recursosBtnMas").toggle();
+      $("#carrera-recursosBtnMenos").toggle();
     });
     // ANIMACION PARA MENU NIVELES
     $("#nivel1-title").click(function() {
