f860dfdeb64a6a949093a5121625acbdc8a66812
JonathanCE <jonatha.CE.2025@protonmail.com>
2022-05-23 00:28:52 -0500

    Se rediseño el codigo de los archivos de un quiz, donde el espaciado es coherente con el resto de lernist. Se solucionaron unos errores en el index, tienda y biblioteca

--- quizzes/general/conocimiento-de-primaria/end.js
+++ quizzes/general/conocimiento-de-primaria/end.js
@@ -39,5 +39,5 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('descubre-europa-con-luisillo.html');
+    window.location.assign('conocimiento-de-primaria.html');
 };
